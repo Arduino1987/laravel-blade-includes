97571883023982ea642df1daa2f67e2da2d19e0b
alexey_zolotariov <alexey_zolotariov@ukrsolution.com>
2018-08-19 14:57:20 +0300

    blade component statements

--- src/BladeServiceProvider.php
+++ src/BladeServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Aghorianducalis\BladeIncludes;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class BladeServiceProvider extends ServiceProvider
@@ -13,11 +14,17 @@ class BladeServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $publishedDirectory = 'includes';
+
         $this->loadViewsFrom(__DIR__.'/views', 'laravel-blade-includes');
 
         $this->publishes([
-            __DIR__.'/views' => resource_path('views/vendor/includes')
+            __DIR__.'/views' => resource_path("views/vendor/$publishedDirectory")
         ]);
+
+        Blade::component("vendor.$publishedDirectory.breadcrumb", 'breadcrumb');
+        Blade::component("vendor.$publishedDirectory.alert_success", 'alertStatus');
+        Blade::component("vendor.$publishedDirectory.alert_error", 'alertError');
     }
 
     /**
